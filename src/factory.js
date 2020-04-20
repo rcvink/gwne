ffcfe3ade7a7ac9456cb25122957655f68b2dca5
rcvink <rrvink@gmail.com>
2020-04-20 01:08:28 +0100

    minor refactor

--- src/factory.js
+++ src/factory.js
@@ -2,59 +2,61 @@ import Matter from "matter-js";
 import MatterAttractors from "matter-attractors";
 
 const createPlanet = (x, y, radius, group, density) => {
-    let options = {
-        isStatic: true,
-        collisionFilter: { group },
-        plugin: { attractors: [ MatterAttractors.Attractors.gravity ]}
-    };
-    let planet = Matter.Bodies.circle(x, y, radius, options);
-    Matter.Body.setDensity(planet, density);
-    return planet;
+  let options = {
+    isStatic: true,
+    collisionFilter: { group },
+    plugin: { attractors: [MatterAttractors.Attractors.gravity] }
+  };
+  let planet = createCircle(x, y, radius, options);
+  Matter.Body.setDensity(planet, density);
+  return planet;
 }
 
 const createCpu = (x, y, length, collisionGroup) =>
-    createStaticRectangle(x, y, length, collisionGroup);
+  createStaticRectangle(x, y, length, collisionGroup);
 
 const createPlayer = (x, y, length, collisionGroup) =>
-    createStaticRectangle(x, y, length, collisionGroup);
+  createStaticRectangle(x, y, length, collisionGroup);
 
 const createRender = (document, canvasId, engine) =>
-    Matter.Render.create({
-        element: document.getElementById(canvasId),
-        engine: engine,
-        options: {
-            width: Math.min(document.documentElement.clientWidth, 1024),
-            height: Math.min(document.documentElement.clientHeight, 720),
-            wireframes: false
-        }
-    });
+  Matter.Render.create({
+    element: document.getElementById(canvasId),
+    engine: engine,
+    options: {
+      width: Math.min(document.documentElement.clientWidth, 1024),
+      height: Math.min(document.documentElement.clientHeight, 720),
+      wireframes: false
+    }
+  });
 
 const createBullet = (fromBody, size, group, density) =>
-    Matter.Bodies.circle(
-        fromBody.position.x,
-        fromBody.position.y,
-        size,
-        { density, collisionFilter: { group } }
-    );
+  createCircle(
+    fromBody.position.x,
+    fromBody.position.y,
+    size,
+    { density, collisionFilter: { group } });
 
 const createBulletForce = (radians, velocity) =>
-    Matter.Vector.mult(createVector(radians), velocity);
+  Matter.Vector.mult(createVector(radians), velocity);
 
 const createStaticRectangle = (x, y, length, group) =>
-    Matter.Bodies.rectangle(x, y, length, length, 
-        { 
-            isStatic: true,
-            collisionFilter: { group }
-         });
+  Matter.Bodies.rectangle(x, y, length, length,
+    {
+      isStatic: true,
+      collisionFilter: { group }
+    });
+
+const createCircle = (x, y, radius, options) =>
+  Matter.Bodies.circle(x, y, radius, options);
 
 const createVector = (radians) =>
-    Matter.Vector.create(Math.cos(radians), Math.sin(radians));
+  Matter.Vector.create(Math.cos(radians), Math.sin(radians));
 
 export const factory = {
-    createPlanet,
-    createCpu,
-    createPlayer,
-    createRender,
-    createBullet,
-    createBulletForce
+  createPlanet,
+  createCpu,
+  createPlayer,
+  createRender,
+  createBullet,
+  createBulletForce
 };
